e3006f29d39799313f757fa7a32edbdc09cd4b95
Surya Jasper <45545431+suryajasper@users.noreply.github.com>
2024-01-28 02:42:24 -0600

    modified:   web/message.js 	modified:   web/server.js

--- web/server.js
+++ web/server.js
@@ -22,27 +22,38 @@ app.post('/session/create', async(req,res) => {
 app.post('/session/add', async(req, res) => {
     try {
         const message = await Message.create(req.body);
-        Session.conversation.push(message);
+        const session = await Session.findById(req.body.sessionId);
+        if (!session)
+        {
+            return res.status(404).json({error: 'Session not found', message: 'Session id does not exist'});
+            session.conversation.push(message);
+            await session.save();
+            res.status(201).json({ success: true, message: 'Message added to the session' });
+        }
     } catch (error) {
-        console.log("Error adding message");
-        res.status(500).json(error.message);
+        console.log("Error adding message", error);
+        res.status(500).json({ error: "Internal Server Error", message: error.message });
     }
 })
 
-app.get('/session/aqquire', async(res,req) => {
+app.get('/session/aqquire/:sessionId', async(req,res) => {
     try {
-        const session = await Session.find({});
+        const session = await Session.findById(req.params.sessionId);
+        if (!session)
+        {
+            return res.status(404).json({ message: 'Session not found' });
+        }
         res.status(200).json(session);
     } catch (error) {
-        console.log("Error aqquiring ");
-        res.status(500).json(error.message);
+        console.log("Error aqquiring session", error.message);
+        res.status(500).json({message: "Internal server error"});
     }
 })
 
-// app.get('/session/createsession', (req, res) => {
-//     res.send('Session Page');
-//     //res.send('Create session');
-// })
+app.get('/session/createsession', (req, res) => {
+    res.send('Session Page');
+    //res.send('Create session');
+})
 
 // app.post('/session/update', async (req, res) => {
 //     try {
@@ -95,7 +106,7 @@ process.on('SIGINT', async () => {
     await closeMongoConnection();
     process.exit(0);
   });
-  
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });

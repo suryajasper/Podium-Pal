a8c5aaa261bf57b0a52d91997dc1d0919297f208
Surya Jasper <45545431+suryajasper@users.noreply.github.com>
2024-01-28 01:36:30 -0600

    modified:   web/dbs.js 	deleted:    web/messageroute.js 	modified:   web/server.js 	modified:   web/session.js 	deleted:    web/test.js

--- web/dbs.js
+++ web/dbs.js
@@ -2,7 +2,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 const Message = require('./message');
-const uri = "mongodb+srv://georgemathew9203:2pP6d3wAimAo1kgy@podiumpalcluster.ufcozb6.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://georgemathew9203:0ffZ6ZrUyAZlyJZW@podiumpalcluster.ufcozb6.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -13,39 +13,74 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
+async function connectToMongo() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
     await mongoose.connect(
       uri,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-      });
-console.log('mongoose connected');
-
-    const newMessage = new Message({
-      author: "George",
-      content: "Text",
-      emotion: "Happy",
-      eye_movement: "Moving",
-      tone: "Straight",
-    });
-
-    console.log('trying to save');
-    await newMessage.save();
-    console.log('saved');
-  } 
-  catch {
-    console.log('fuck');
-  }
-  finally {
-    await client.close();
+      }
+    );
+    console.log('Mongoose connected');
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
   }
 }
-run().catch(console.dir);
+
+async function closeMongoConnection() {
+  try {
+    await client.close();
+    //console.log("MongoDB connection closed");
+    await mongoose.connection.close();
+    console.log("MongoDB connections closed");
+  } catch (error) {
+    console.error("Error closing MongoDB connection:", error);
+  }
+}
+
+module.exports = { connectToMongo, closeMongoConnection, Message };
+
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+//     await mongoose.connect(
+//       uri,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       });
+// console.log('mongoose connected');
+
+//     const newMessage = new Message({
+//       author: "George",
+//       content: "Text",
+//       emotion: "Happy",
+//       eye_movement: "Moving",
+//       tone: "Straight",
+//     });
+
+//     console.log('trying to save');
+//     await newMessage.save();
+//     console.log('saved');
+//   } 
+//   catch (error){
+//     console.log('no', error);
+//   }
+//   finally {
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
--- web/server.js
+++ web/server.js
@@ -2,44 +2,77 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const Message = require('./message');
-const Session = require('./message');
+const Session = require('./session');
+const { connectToMongo, closeMongoConnection } = require('./dbs');
+
 
 app.use(express.json());
 
-app.get('/session/createsession', (req, res) => {
-    res.send('Session Page');
-    //res.send('Create session');
-})
+connectToMongo();
 
-app.post('/session/update', async (req, res) => {
+app.post('/session/create', async(req,res) => {
     try {
-        let id = req.params.id;
-        if (!id) {
-            console.log('no id');
-            return res.status(400).json({ message: 'ID Error' });
-        }
-        const existingMessage = await Message.findById(id);
-
-        console.log('saving');
-        const updatedMessage =await existingMessage.save();
-        if (req.body.sessionId) {
-            await Session.updateOne(
-                { _id: req.body.sessionId },
-                { $set: { 'response.$': updatedMessage } }
-            );
-            session.response.push(updatedMessage);
-        }
-        res.status(200).json(updatedMessage);
-    } catch(error) {
-        res.status(400).json({message: error.message})
+        const session = await Session.create(req.body);
+    } catch (error) {
+        console.log("Error creating session", error);
+        res.status(500).json({ error: "Internal Server Error", message: error.message });
     }
-});
-
-
-
-app.get('/message', (req, res) => {
-    res.send('Message Page');
 })
+
+app.post('/session/add', async(req, res) => {
+    try {
+        const message = await Message.create(req.body);
+        Session.conversation.push(message);
+    } catch (error) {
+        console.log("Error adding message");
+        res.status(500).json(error.message);
+    }
+})
+
+app.get('/session/aqquire', async(res,req) => {
+    try {
+        const session = await Session.find({});
+        res.status(200).json(session);
+    } catch (error) {
+        console.log("Error aqquiring ");
+        res.status(500).json(error.message);
+    }
+})
+
+// app.get('/session/createsession', (req, res) => {
+//     res.send('Session Page');
+//     //res.send('Create session');
+// })
+
+// app.post('/session/update', async (req, res) => {
+//     try {
+//         let id = req.params.id;
+//         if (!id) {
+//             console.log('no id');
+//             return res.status(400).json({ message: 'ID Error' });
+//         }
+//         const existingMessage = await Message.findById(id);
+
+//         console.log('saving');
+//         const updatedMessage =await existingMessage.save();
+//         if (req.body.sessionId) {
+//             await Session.updateOne(
+//                 { _id: req.body.sessionId },
+//                 { $set: { 'response.$': updatedMessage } }
+//             );
+//             session.response.push(updatedMessage);
+//         }
+//         res.status(200).json(updatedMessage);
+//     } catch(error) {
+//         res.status(400).json({message: error.message})
+//     }
+// });
+
+
+
+// app.get('/message', (req, res) => {
+//     res.send('Message Page');
+// })
 // // Define a simple GET endpoint
 // app.get('/', (req, res) => {
 //     res.send('Hello, World!');
@@ -58,6 +91,11 @@ app.get('/message', (req, res) => {
 // });
 
 // Start the server
+process.on('SIGINT', async () => {
+    await closeMongoConnection();
+    process.exit(0);
+  });
+  
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
